88739b8bfeb67571d84bf083f9b70b7c44d28ca0
harshil6-lab <harshilkalsariya6@gmail.com>
2026-01-15 23:06:32 +0530

    Add - logic of backoff in Production file

--- API/production.py
+++ API/production.py
@@ -5,11 +5,13 @@ from config_loader import config_loader
 import json
 import requests
 from logger import logging_config
+import time
 
 load_dotenv()
 config = config_loader()
 logging_config()
 MAX_RETRIES = config["maximum_retry"]
+sleep_time = config["sleep_time"]
 
 ENV = os.getenv("API_URL")
 def checkAPI():
@@ -23,8 +25,14 @@ def checkAPI():
             else:
                 raise Exception("API call failed with status code: %s",format(response.status_code))
         except Exception as e:
+
           logging.error("Error occurred during API call: %s",str(e))
+           #backoff 
+          time.sleep(sleep_time)
+          sleep_time *=2
+          logging.info("Sleeping for %s seconds before retrying",sleep_time)
           logging.info("Retrying API call, attempt %s",attempt)
+
     logging.critical("Max retries reached. API call is unsuccessful.",)
     return None
 
